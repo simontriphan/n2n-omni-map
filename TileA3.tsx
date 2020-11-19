e2ad5eb7382f15d6b4dd34b034ba2bb847457e88
omnicide <omnicide@omnicide.de>
2020-11-19 20:00:08 +0100

    Futuristic Tileset, test map

---
+++ TileA3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="TileA3" tilewidth="16" tileheight="16" tilecount="512" columns="32">
+ <image source="../../../../../Program Files (x86)/Steam/steamapps/common/RPGVXAce/dlc/Futuristic Tiles/System/TileA3.png" width="512" height="256"/>
+</tileset>
---
+++ TileA4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="TileA4" tilewidth="16" tileheight="16" tilecount="960" columns="32">
+ <image source="../../../../../Program Files (x86)/Steam/steamapps/common/RPGVXAce/dlc/Futuristic Tiles/System/TileA4.png" width="512" height="480"/>
+</tileset>
